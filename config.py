4918d78b499bc95653caa5af864f18b9f60fc3e9
Yz-gre <matthew.y.zhang@gmail.com>
2024-08-04 15:49:05 -0400

    changing locations of files for better push pull dynamic

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-MZFilePath = '/home/matthew_y_zhang/GREBot/TransactionsMZ.csv'
-NTFilePath = '/home/matthew_y_zhang/GREBot/TransactionsNT.csv'
+MZFilePath = '/home/matthew_y_zhang/TransactionsMZ.csv'
+NTFilePath = '/home/matthew_y_zhang/TransactionsNT.csv'
 #MZFilePath = 'C:\Users\matth\OneDrive\GRE_Server_Support\TransactionsMZ.csv'
 #NTFilePath = 'C:\Users\matth\OneDrive\GRE_Server_Support\TransactionsNT.csv'
