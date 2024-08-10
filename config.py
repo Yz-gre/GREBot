fda2d090a617fd84e7693dbb3a81e9808f39a2cc
Yz-gre <matthew.y.zhang@gmail.com>
2024-08-09 23:34:23 -0400

    updating config file like i should've done

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-#MZFilePath = '/home/matthew_y_zhang/TransactionsMZ.csv'
-MZFilePath = "C:/Users/matth/OneDrive/GRE_Server_Support/TransactionsMZ.csv"
+MZFilePath = '/home/matthew_y_zhang/TransactionsMZ.csv'
+#MZFilePath = "C:/Users/matth/OneDrive/GRE_Server_Support/TransactionsMZ.csv"
 MZLOCLimit = 163600
 MZLOCUsage = 91193.85
-#NTFilePath = '/home/matthew_y_zhang/TransactionsNT.csv'
-NTFilePath = "C:/Users/matth/OneDrive/GRE_Server_Support/TransactionsNT.csv"
+NTFilePath = '/home/matthew_y_zhang/TransactionsNT.csv'
+#NTFilePath = "C:/Users/matth/OneDrive/GRE_Server_Support/TransactionsNT.csv"
 NTLOCLimit = 460000
 NTLOCUsage = 220000
